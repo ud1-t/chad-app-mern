7927687c3a4a60f7ec9781efd7ee63de1b576128
ud1-t <udittripathi0101@gmail.com>
2024-03-24 19:53:41 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -10,6 +11,8 @@ import { app, server } from './socket/socket.js'
 
 const PORT = process.env.PORT || 4000
 
+const __dirname = path.resolve()
+
 dotenv.config()
 
 app.use(express.json())     // to parse the incoming requests wiht JSON payloads (from req.body)
@@ -19,10 +22,12 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-app.get("/", (req, res) => {
-    // root route http://localhost:4000/
-    res.send("Hello World!!")
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"))
 })
+
 
 
 server.listen(PORT, () => {
